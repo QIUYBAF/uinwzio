0b017fde3fe00583568b48e903fb5b973d0e6a5c
QIUYBAF <caculus314@gmail.com>
2026-09-03 20:28:12 +0800

    feat(agentcut4): expose Director identity and CutGraph API

---
+++ AgentCut/Director4/src/agentcut_director/__init__.py
@@ -0,0 +1,5 @@
+from .identity import PRODUCT_IDENTITY, identity_payload
+from .cutgraph import CutGraph, CutGraphError
+
+__version__ = "4.0.0"
+__all__ = ["__version__", "PRODUCT_IDENTITY", "identity_payload", "CutGraph", "CutGraphError"]
