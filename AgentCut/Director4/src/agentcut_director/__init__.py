30e6d0d0703df8714435ca17eee3604d7fee2852
QIUYBAF <caculus314@gmail.com>
2026-09-03 22:42:59 +0800

    feat(agentcut4): expose Director 4 public API

--- AgentCut/Director4/src/agentcut_director/__init__.py
+++ AgentCut/Director4/src/agentcut_director/__init__.py
@@ -1,5 +1,33 @@
-from .identity import PRODUCT_IDENTITY, identity_payload
-from .cutgraph import CutGraph, CutGraphError
+"""AgentCut Director 4 public API."""
 
-__version__ = "4.0.0"
-__all__ = ["__version__", "PRODUCT_IDENTITY", "identity_payload", "CutGraph", "CutGraphError"]
+from .identity import PRODUCT_NAME, VERSION, product_identity
+from .cutgraph import (
+    CutGraphError,
+    canonical_json,
+    load_project,
+    new_project,
+    project_hash,
+    save_project,
+    validate_project,
+)
+from .operations import ConflictError, apply_transaction, preflight, undo_last
+from .remotion import export_remotion_bundle, verify_remotion_bundle
+
+__all__ = [
+    "PRODUCT_NAME",
+    "VERSION",
+    "product_identity",
+    "CutGraphError",
+    "ConflictError",
+    "canonical_json",
+    "load_project",
+    "new_project",
+    "project_hash",
+    "save_project",
+    "validate_project",
+    "apply_transaction",
+    "preflight",
+    "undo_last",
+    "export_remotion_bundle",
+    "verify_remotion_bundle",
+]
